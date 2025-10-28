cc97713ff6f378f7dc75abb1966c5b3c326b6805
careserverziac <careserverziac@gmail.com>
2025-10-28 15:05:21 +0530

    First commit

---
+++ Sampledata/Sampledata/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sampledata.Models;
+namespace Sampledata.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            var students = new List<Student>
+            {
+                new Student{Id=1,Name="dawood"},
+                 new Student{Id=1,Name="dawood"}
+            };
+            return View(students);
+        }
+    }
+}
